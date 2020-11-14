b20222ca1e84fd13d34d2c6400d73613b9f2fdde
jguyet <gygydu19@hotmail.fr>
2020-11-14 05:04:18 +0100

    ji generate index, ji generate controller, ji generate controller --index, coverage at 80%

--- test/run-arguments.js
+++ test/run-arguments.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const { getStreamChunk } = require('../lib/utils/test.util');
 const del = require('del');
+const sepCase = require('../lib/utils/sepCase');
 
 if (fs.existsSync(path.resolve(__dirname, '../tap-test'))) {
   try {
@@ -23,6 +24,82 @@ const execCommand = (cmds) => {
   return spawn(process.execPath, arrayArgs);
 };
 
+test('run arguments not found', function (t) {
+  t.plan(1);
+
+  let ps = execCommand(['ninja', `--path="${path.resolve(__dirname, '../tap-test')}"`]);
+  let errorStream = getStreamChunk();
+  let outStream = getStreamChunk();
+  
+  ps.stderr.pipe(errorStream.stream);
+  ps.stdout.pipe(outStream.stream);
+  
+  ps.on('exit', function (code) {
+    if (!errorStream.chunks.join("\n").includes("Command ninja not found.")) {
+      t.fail("run arguments not found");
+    }
+    t.pass("ok");
+    t.end();
+  });
+});
+
+test('run arguments --config not exists', function (t) {
+  t.plan(1);
+
+  let ps = execCommand(['build', "--config='./toto.json'", `--path="${path.resolve(__dirname, '../tap-test')}"`]);
+  let errorStream = getStreamChunk();
+  let outStream = getStreamChunk();
+  
+  ps.stderr.pipe(errorStream.stream);
+  ps.stdout.pipe(outStream.stream);
+  
+  ps.on('exit', function (code) {
+    if (!errorStream.chunks.join("\n").includes("Command build need jiconfig.json")) {
+      t.fail("run arguments --config not exists");
+    }
+    t.pass("ok");
+    t.end();
+  });
+});
+
+test('run arguments --port not a number', function (t) {
+  t.plan(1);
+
+  let ps = execCommand(['--port=bou']);
+  let errorStream = getStreamChunk();
+  let outStream = getStreamChunk();
+  
+  ps.stderr.pipe(errorStream.stream);
+  ps.stdout.pipe(outStream.stream);
+  
+  ps.on('exit', function (code) {
+    if (!errorStream.chunks.join("\n").includes("--port need number value.")) {
+      t.fail("run arguments --port not a number");
+    }
+    t.equal(code, 0);
+    t.end();
+  });
+});
+
+test('run arguments empty', function (t) {
+  t.plan(1);
+
+  let ps = execCommand([]);
+  let errorStream = getStreamChunk();
+  let outStream = getStreamChunk();
+  
+  ps.stderr.pipe(errorStream.stream);
+  ps.stdout.pipe(outStream.stream);
+  
+  ps.on('exit', function (code) {
+    if (!errorStream.chunks.join("\n").includes("Please specify a single command.")) {
+      t.fail("run arguments empty");
+    }
+    t.equal(code, 0);
+    t.end();
+  });
+});
+
 test('run arguments --help', function (t) {
     t.plan(1);
 
@@ -38,7 +115,7 @@ test('run arguments --help', function (t) {
         t.fail("--help not working");
       }
       t.equal(code, 0);
-      t.end()
+      t.end();
     });
 });
 
@@ -57,7 +134,7 @@ test('run arguments --version', function (t) {
       t.fail("--version not working");
     }
     t.equal(code, 0);
-    t.end()
+    t.end();
   });
 });
 
@@ -78,8 +155,8 @@ test('run arguments new', function (t) {
     if (!fs.existsSync(path.resolve(__dirname, '../tap-test/node_modules'))) {
       t.fail("ji npm install project fail");
     }
-    t.pass("ok")
-    t.end()
+    t.pass("ok");
+    t.end();
   });
 });
 
@@ -97,7 +174,154 @@ test('run arguments build', function (t) {
     if (!fs.existsSync(path.resolve(__dirname, '../tap-test/dist'))) {
       t.fail("ji npm install project fail");
     }
-    t.pass("ok")
-    t.end()
+    t.pass("ok");
+    t.end();
+  });
+});
+
+test('run arguments debug', function (t) {
+  t.plan(1);
+
+  let ps = execCommand(['debug', `--path="${path.resolve(__dirname, '../tap-test')}"`]);
+  let errorStream = getStreamChunk();
+  let outStream = getStreamChunk();
+  
+  ps.stderr.pipe(errorStream.stream);
+  ps.stdout.pipe(outStream.stream);
+
+  setTimeout(() => spawn('kill', ['-3', ps.pid + '']), 2000);
+  
+  ps.on('exit', function (code) {
+    t.pass("ok");
+    t.end();
+  });
+});
+
+
+test('run arguments generate not found', function (t) {
+  t.plan(1);
+
+  let ps = execCommand(['generate', 'nan', 'test-index', `--path="${path.resolve(__dirname, '../tap-test')}"`]);
+  let errorStream = getStreamChunk();
+  let outStream = getStreamChunk();
+  
+  ps.stderr.pipe(errorStream.stream);
+  ps.stdout.pipe(outStream.stream);
+  
+  ps.on('exit', function (code) {
+    console.log(outStream.chunks.join("\n"));
+    if (!errorStream.chunks.join("\n").includes('Generate Type nan not found.')) {
+      t.fail("argument generate works should be KO");
+    }
+    t.pass("ok");
+    t.end();
+  });
+});
+
+test('run arguments generate not found 2', function (t) {
+  t.plan(1);
+
+  let ps = execCommand(['generate', `--path="${path.resolve(__dirname, '../tap-test')}"`]);
+  let errorStream = getStreamChunk();
+  let outStream = getStreamChunk();
+  
+  ps.stderr.pipe(errorStream.stream);
+  ps.stdout.pipe(outStream.stream);
+  
+  ps.on('exit', function (code) {
+    if (!errorStream.chunks.join("\n").includes('Generate Type not found.')) {
+      t.fail("Generate Type not found. should be KO");
+    }
+    t.pass("ok");
+    t.end();
+  });
+});
+
+
+test('run arguments generate index', function (t) {
+  t.plan(1);
+
+  let ps = execCommand(['generate', 'index', 'test-index', `--path="${path.resolve(__dirname, '../tap-test')}"`]);
+  let errorStream = getStreamChunk();
+  let outStream = getStreamChunk();
+  
+  ps.stderr.pipe(errorStream.stream);
+  ps.stdout.pipe(outStream.stream);
+  
+  ps.on('exit', function (code) {
+    if (!fs.existsSync(path.resolve(__dirname, '../tap-test/src/test-index/test-index.index.js'))) {
+      t.fail("ji ng generate index fail");
+    }
+    t.pass("ok");
+    t.end();
+  });
+});
+
+test('run arguments generate controller', function (t) {
+  t.plan(1);
+
+  let ps = execCommand(['generate', 'controller', 'toto', `--path="${path.resolve(__dirname, '../tap-test')}"`]);
+  let errorStream = getStreamChunk();
+  let outStream = getStreamChunk();
+  
+  ps.stderr.pipe(errorStream.stream);
+  ps.stdout.pipe(outStream.stream);
+  
+  ps.on('exit', function (code) {
+    if (!fs.existsSync(path.resolve(__dirname, '../tap-test/src/toto/toto.controller.js'))) {
+      t.fail("ji ng generate controller fail");
+    }
+    t.pass("ok");
+    t.end();
+  });
+});
+
+test('run arguments generate controller --index', function (t) {
+  t.plan(1);
+
+  let ps = execCommand(['generate', 'controller', 'toto', '--index', `--path="${path.resolve(__dirname, '../tap-test')}"`]);
+  let errorStream = getStreamChunk();
+  let outStream = getStreamChunk();
+  
+  ps.stderr.pipe(errorStream.stream);
+  ps.stdout.pipe(outStream.stream);
+  
+  ps.on('exit', function (code) {
+    if (!fs.existsSync(path.resolve(__dirname, '../tap-test/src/toto/toto.controller.js'))) {
+      t.fail("ji ng generate controller fail");
+    }
+    t.pass("ok");
+    t.end();
+  });
+});
+
+
+test('test sepCase', function (t) {
+  t.plan(1);
+
+  t.equal(sepCase("Absiccee___1"), "absiccee-1");
+  t.end();
+});
+
+
+test('run arguments build with deleted favicon', function (t) {
+  t.plan(1);
+
+  del.sync(path.resolve(__dirname, '../tap-test/dist'));
+  del.sync(path.resolve(__dirname, '../tap-test/src/favicon.ico'));
+
+  let ps = execCommand(['build', `--path="${path.resolve(__dirname, '../tap-test')}"`]);
+  let errorStream = getStreamChunk();
+  let outStream = getStreamChunk();
+  
+  ps.stderr.pipe(errorStream.stream);
+  ps.stdout.pipe(outStream.stream);
+  
+  ps.on('exit', function (code) {
+    if (fs.existsSync(path.resolve(__dirname, '../tap-test/dist/favicon.ico'))) {
+      t.fail("run arguments build with deleted favicon");
+    }
+    t.pass("ok");
+    t.end();
   });
 });
